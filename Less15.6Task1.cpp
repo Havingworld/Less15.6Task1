84827621e295bf590a0e516a2bcf4363620db4a4
Владимир Тетерин <havingworld@gmail.com>
2022-02-07 11:47:43 +1000

    цикл в один проход

--- Less15.6Task1.cpp
+++ Less15.6Task1.cpp
@@ -16,17 +16,46 @@ using namespace std;
 int main()
 {
     vector <int> a{ -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-    int nI, nJ, nBigger =0;
-    for (int i = 0; i < a.size()-1; i++) {
+    int nI = 0, nJ = 1, /* nBigger = 0,*/ nCalc = a[0] + a[1], nCalcTemp = 0;
+    bool incRight = true;
+    for (int i = 0, j = 1; j < a.size() && i<j;)
+    {
+        if (incRight)
+        {
+            nCalcTemp += a[j++];
+        }
+        else
+        {
+            nCalcTemp -= a[i++];
+        }
+
+        if (nCalcTemp > nCalc)
+        {
+            nCalc = nCalcTemp;
+            nI = i;
+            nJ = j;
+            incRight = !incRight;
+        }
+            
+    }
+
+   
+    /*
+   for (int i = 0; i < a.size() - 1; i++)
+    {
         int nCalc = a[i];
-        for (int j = i + 1; j < a.size(); j++) {
+        for (int j = i + 1; j < a.size(); j++) 
+        {
             nCalc += a[j];
-            if (nCalc > nBigger) {
+            if (nCalc > nBigger) 
+            {
                 nBigger = nCalc;
                 nI = i;
                 nJ = j;
             }
         }
     }
+   */ 
+    
     cout << nI << " " << nJ;
 }
